402812a13173ea9391cc18089e4a44b4ed04c6b5
solfegio <your_email@whatever.com>
2026-01-14 21:56:31 +0300

    v1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import './index.css'
+import Board from './components/Board';
+import { useState } from 'react';
+
+function App() {
+
+
+  return (
+    <div id='main-wrapper'>
+      
+      <Board/>
+    </div>
+  )
+}
+
+export default App  
